4f8cd0210564ce1ebeb3390475ac419394805341
CRISTIAN COTRENA <13104405@pucrs.br>
2018-05-15 22:33:46 -0300

    ahhhh

--- test/NewEmptyJUnitTest.java
+++ test/NewEmptyJUnitTest.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 
+import business.Calculos;
+import business.Contribuinte;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,28 +19,38 @@ import static org.junit.Assert.*;
  */
 public class NewEmptyJUnitTest {
     
-    public NewEmptyJUnitTest() {
+    @Test
+    public void calc_IRPF_Normal(){
+        String response, test;
+        test     = "[0.0, 1066.5, 1022.625, 1124.8545, 993.375, 31472.4375]";
+        
+        
+        //RESULTADO < 12.000
+        Contribuinte contribuinte = new Contribuinte("Icaro", "08755548909", 60, 5, 500, 3_000);
+        response = "[" + Calculos.calculaIRPF(true, contribuinte);
+        //60 ANOS, 2 DEPENDENTES, 500 CONTRIBUICAO, 20_000 RENDIMENTO
+        Contribuinte contribuinte2 = new Contribuinte("Icaro", "08755548909", 60, 2, 500, 20_000);
+        response = response + ", " + Calculos.calculaIRPF(true, contribuinte2);
+        //60 ANOS, 4 DEPENDENTES, 500 CONTRIBUICAO, 20_000 RENDIMENTO
+        Contribuinte contribuinte3 = new Contribuinte("Icaro", "08755548909", 60, 4, 500, 20_000);
+        response = response + ", " + Calculos.calculaIRPF(true, contribuinte3);
+        //70 ANOS, 2 DEPENDENTES, 500 CONTRIBUICAO, 20_000 RENDIMENTO
+        Contribuinte contribuinte4 = new Contribuinte("Icaro", "08755548909", 70, 2, 500, 20_000);
+        response = response + ", " + Calculos.calculaIRPF(true, contribuinte4);
+        //70 ANOS, 4 DEPENDENTES, 500 CONTRIBUICAO, 20_000 RENDIMENTO
+        Contribuinte contribuinte5 = new Contribuinte("Icaro", "08755548909", 70, 4, 500, 20_000);
+        response = response + ", " + Calculos.calculaIRPF(true, contribuinte5);
+        //70 ANOS, 4 DEPENDENTES, 500 CONTRIBUICAO, 50_000 RENDIMENTO
+        Contribuinte contribuinte6 = new Contribuinte("Icaro", "08755548909", 70, 4, 500, 50_000);
+        response = response + ", " + Calculos.calculaIRPF(true, contribuinte6) + "]";
+        
+        //double x = ((((20_000 - 500)*0.98) - 12_000) * 0.15);
+         
+        assertEquals(test, response);
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
+    @Test
+    public void calc_IRPF_Simplificado(){
+        Contribuinte contribuinte = new Contribuinte("Selenia", "08204637157", 100, 4000);
+        assertEquals(0, 0);
     }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
